1e25f4429b99afdd4a9c44146f07a1858c9f2724
Maxim <maxmosk@list.ru>
2022-10-22 18:15:34 +0300

    Add logging to debug file

--- src/include/debug.h
+++ src/include/debug.h
@@ -22,7 +22,7 @@ while (0)
 #define ASSERT(condition, message)               \
 do                                                \
 {                                                  \
-    if (!condition)                                 \
+    if (!(condition))                               \
     {                                                \
         fprintf(stderr, "*** ASSERTION ERROR ***\n"); \
         fprintf(stderr, "    Failed %s\n", message);   \
@@ -36,6 +36,30 @@ do                                                \
 while (0)
 #else
 #define ASSERT(condition, message) do {} while (0)
+#endif /* NDEBUG */
+
+#ifndef NDEBUG_LOG
+#define LOGOPEN()                                               \
+FILE *log_opened = fopen("log.txt", "a");                        \
+do                                                                \
+{                                                                  \
+    ASSERT(NULL != log_opened, "Log file opening");                 \
+    LOGPRINTF("\nLog start  in file %32s, function %32s, line %6d\n",\
+                                    __FILE__, __func__, __LINE__);    \
+}                                                                      \
+while (0)
+
+#define LOGPRINTF(...) fprintf(log_opened, __VA_ARGS__)
+
+#define LOGCLOSE()                                              \
+do                                                               \
+{                                                                 \
+    LOGPRINTF("Log finish in file %32s, function %32s, line %6d\n",\
+                                    __FILE__, __func__, __LINE__);  \
+    fclose(log_opened);                                              \
+}                                                                     \
+while (0)
+#else
 #endif
 
 #endif /* DEBUG_H_INCLUDED__ */
